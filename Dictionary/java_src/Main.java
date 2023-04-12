a2656a9b347f3717b990ea0789e9c7d9f8964ede
archonus <excalibur.archonus@gmail.com>
2023-04-12 20:38:40 +0100

    Implemented insertion

--- Dictionary/java_src/Main.java
+++ Dictionary/java_src/Main.java
@@ -2,5 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        var rbt = new RedBlackTree<Integer,String>();
+        var n = 50;
+        for (Integer i = n; i >= 0; i--) {
+            rbt.set(i,i.toString());
+        }
+        try {
+            System.out.println((rbt.retrieve(24)));
+            System.out.println(rbt.retrieve(3));
+        }
+        catch (InvalidKeyException e){
+            System.out.println("Invalid key exception thrown");
+        }
     }
 }
--- Dictionary/java_src/RedBlackTree.java
+++ Dictionary/java_src/RedBlackTree.java
@@ -1,9 +1,11 @@
 class RedBlackTree<K extends Comparable<K>, V>{
 
-    class RBNode{
+    private class RBNode{
         private K key;
         private V value;
         private boolean isBlack;
+
+        private RBNode parent = null;
 
         private RBNode left = NIL;
         private  RBNode right = NIL;
@@ -38,26 +40,111 @@ class RedBlackTree<K extends Comparable<K>, V>{
             this.isBlack = isBlack;
         }
 
-        public RBNode getLeft() {
+        public RBNode getLeftChild() {
             return left;
         }
 
-        public void setLeft(RBNode left) {
+        public void setLeftChild(RBNode left) {
             this.left = left;
+            if(left != NIL){
+                left.parent = this;
+            }
+
         }
 
-        public RBNode getRight() {
+        public RBNode getRightChild() {
             return right;
         }
 
-        public void setRight(RBNode right) {
+        public void setRightChild(RBNode right) {
             this.right = right;
+            if(right != NIL){
+                right.parent = this;
+            }
+        }
+
+        public RBNode getParent() {
+            return parent;
+        }
+
+        public void setParent(RBNode parent) {
+            if(this != NIL){  // NIL has no parent
+                this.parent = parent;
+            }
+        }
+
+        public boolean isLeftChild(){
+            if (this.parent == NIL){
+                return false;
+            }
+            var comp = this.key.compareTo(this.parent.key);
+            return comp < 0;
+        }
+
+        public void leftRotate() throws IllegalArgumentException{
+            if(this.right == NIL){
+                throw new IllegalArgumentException("Node has no right child");
+            }
+           var right_child = this.right;
+           this.setRightChild(right_child.left);
+
+            //Update parent pointer
+            if(this.parent == NIL){
+                RedBlackTree.this.setRoot(right_child);
+            }
+            else{
+                if(this.isLeftChild()){
+                    this.parent.setLeftChild(right_child);
+                }
+                else{
+                    this.parent.setRightChild(right_child);
+                }
+            }
+
+            right_child.setLeftChild(this);
+        }
+
+        public void rightRotate() throws IllegalArgumentException{
+            if(this.left == NIL){
+                throw new IllegalArgumentException("Node has no left child");
+            }
+            var left_child = this.left;
+            this.setLeftChild(left_child.right);
+
+
+            //Update parent pointer
+            if(this.parent == NIL){ // Root
+                RedBlackTree.this.setRoot(left_child);
+            }
+            else{
+                if(this.isLeftChild()){
+                    this.parent.setLeftChild(left_child);
+                }
+                else{
+                    this.parent.setRightChild(left_child);
+                }
+            }
+            left_child.setRightChild(this);
+        }
+
+        @Override
+        public String toString() {
+            if(this == NIL){
+                return "RBNode{NIL}";
+            }
+            return "RBNode{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    ", isBlack=" + isBlack +
+                    '}';
         }
     }
-    private final RBNode NIL = new RBNode(null, null);
+    private final RBNode NIL;
 
     private RBNode root;
     public RedBlackTree() {
+        NIL = new RBNode(null, null);
+        NIL.setIsBlack(true);
         this.root = NIL;
     }
 
@@ -69,13 +156,20 @@ class RedBlackTree<K extends Comparable<K>, V>{
                 return current;
             }
             else if (comp < 0) {
-                current = current.getLeft();
+                current = current.getLeftChild();
             }
             else{
-                current = current.getRight();
+                current = current.getRightChild();
             }
         }
         throw new InvalidKeyException();
+
+    }
+
+    public void setRoot(RBNode node) {
+        this.root = node;
+        node.parent = NIL; // Only the root node has a parent of NIL
+        node.isBlack = true;
 
     }
 
@@ -85,29 +179,103 @@ class RedBlackTree<K extends Comparable<K>, V>{
 
     }
 
-    public void set(K key, V newValue) throws InvalidKeyException {
-        var node = find_node(key);
-        node.setValue(newValue);
+    private void fix(RBNode node){
+        if(node.getParent().isBlack() && !node.isBlack()){ // There is no violation, since the node is red
+            return;
+        }
+        RBNode current = node;
+        RBNode p = node.getParent();
+        while(!p.isBlack()){ // p is red
+            var g = p.getParent(); // p is red, so cannot be root, so g exists and is black
+            RBNode u;
+            if(p.isLeftChild()){ // p is left child of g
+                u = g.getRightChild();
+                if(u.isBlack()){ // u is a black child of the red g
+                    if(!current.isLeftChild()){ // Current is right child of parent, so swap them (since both are red)
+                        p.leftRotate(); // Current is now left child of g, and p is a child of current
+                        var temp = current;
+                        current = p;
+                        p = temp;
+                        // Fall through to the next case, since now current is left child of p, and both are red
+                    }
+                    p.setIsBlack(true); // p is black, so loop will terminate
+                    g.setIsBlack(false); // g is now red
+
+                    g.rightRotate();
+                }
+                else{ // u is red and therefore not NIL
+                    p.setIsBlack(true);
+                    u.setIsBlack(true);
+                    g.setIsBlack(false);
+
+                    // Moved problem up to g, so need to fix that
+
+                    current = g;
+                    p = g.getParent();
+                }
+            }
+            else{ // p is right child of g
+                u = g.getLeftChild();
+                if(u.isBlack()){
+                    if(current.isLeftChild()){ // Current is left child
+                        p.rightRotate(); // Make p the right child of current
+
+                        // Still two red nodes, but now they make a right connection
+                        var temp = current;
+                        current = p;
+                        p = temp;
+                    }
+                    p.setIsBlack(true); // p is black, so loop will terminate
+                    g.setIsBlack(false); // g is now red
+
+                    g.leftRotate();
+                }
+                else{ // u is red and therefore not NIL
+                    p.setIsBlack(true);
+                    u.setIsBlack(true);
+                    g.setIsBlack(false);
+
+                    current = g;
+                    p = g.getParent();
+                }
+
+            }
+        }
+        this.root.setIsBlack(true);
+
     }
 
-    public void insert(K key, V value) throws InvalidKeyException {
+    public void set(K key, V value) {
         var node = new RBNode(key,value);
         if (this.root == NIL){
-            this.root = node;
+            this.setRoot(node);
         }
         else{
             var current = this.root;
             RBNode parent = null;
+            int comp = 0;
             while(current != NIL){
                 if(current.getKey().equals(key)){
-                    throw new InvalidKeyException();
+                    current.setValue(value);
+                    return;
                 }
-                var comp = current.getKey().compareTo(key);
+                comp = key.compareTo(current.getKey());
                 parent = current;
                 if(comp < 0){
-
+                    current = current.getLeftChild();
+                }
+                else{
+                    current = current.getRightChild();
                 }
             }
+
+            if(comp < 0){
+                parent.setLeftChild(node);
+            }
+            else{
+                parent.setRightChild(node);
+            }
+            fix(node);
         }
     }
 }

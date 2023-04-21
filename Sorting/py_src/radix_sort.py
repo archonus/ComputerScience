492a55c520940f3fc411966fdf4fd1e452adefc3
archonus <excalibur.archonus@gmail.com>
2023-04-21 17:41:45 +0100

    Implemented radix sort

--- Sorting/py_src/radix_sort.py
+++ Sorting/py_src/radix_sort.py
@@ -2,6 +2,7 @@ from typing import Any, Callable, List, TypeVar
 T = TypeVar('T')
 
 def _counting_sort(items : List[T], k, key : Callable[[T], int] = lambda x : x):
+    """Sort on assumption that key(item) <= k for all items. Default is to expect a list of numbers"""
     n = len(items)
     counts = [0 for _ in range(k+1)]
     output = [None for _ in range(n)]
@@ -26,8 +27,20 @@ def _counting_sort(items : List[T], k, key : Callable[[T], int] = lambda x : x):
 
     return output
 
+def get_digit(n, i):
+    return n // (10**i)
+
+def get_char(s, i):
+    return ord(s[-(i + 1)])
+
+def radix_sort(items : T, d : int, k : int, get_digit : Callable[[T, int], int] = get_digit):
+    for i in range(d):
+        key = lambda x : get_digit(x,i)
+        items = _counting_sort(items,k,key)
+    return items
+
 if __name__ == '__main__':
-    ls = [3,4,5,1,6,2,7,3,2,9,9,3]
-    s = _counting_sort(ls, 9)
+    ls = [123,432,512,11,667,22,790,32,245,92,97,31,31,512,657,5]
+    s = radix_sort(ls,3, 9)
     print(s)
     

6f97f19163f78d2b8213093470aadda69622c995
archonus <excalibur.archonus@gmail.com>
2023-04-22 13:29:52 +0100

    Implemented LCS with input and output and fixed indexing problem

--- DynamicProgramming/src/Main.java
+++ DynamicProgramming/src/Main.java
@@ -1,6 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        LCS.longestCommonSubsequence("hello", "world");
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        lcsMain();
+    }
+
+    public static void lcsMain() throws IOException{
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter first string...");
+        var xs = br.readLine();
+        System.out.print("Enter second string...");
+        var ys = br.readLine();
+        var s= LCS.longestCommonSubsequence(xs,ys);
+        System.out.println(s);
     }
 }

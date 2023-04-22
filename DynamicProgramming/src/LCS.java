6b7d7a5019931e55f16b0d4ece82ffea7d0f928b
archonus <excalibur.archonus@gmail.com>
2023-04-22 10:09:18 +0100

    Started LCS.java

---
+++ DynamicProgramming/src/LCS.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class LCS {
+    public static  String longestCommonSubsequence(String xs, String ys){
+        return "";
+    }
+}
---
+++ DynamicProgramming/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        LCS.longestCommonSubsequence("hello", "world");
+        System.out.println("Hello world!");
+    }
+}

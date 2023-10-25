4c960be12ea4c83b2b770f1c8b642f167dc2c6ee
archonus <excalibur.archonus@gmail.com>
2023-10-25 09:41:32 +0100

    Started XORList foreach

--- XORList/xorlist.c
+++ XORList/xorlist.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 typedef struct xlistNode
 {
     int value;
@@ -21,12 +22,16 @@ XORList *createEmptyList()
     return ls;
 }
 
+static Node* ptr_xor(Node* a, Node * b){
+    return (Node*) ((uintptr_t) a ^ (uintptr_t) b);
+}
+
 bool isEmpty(XORList *ls)
 {
     return ls->start == NULL;
 }
 
-void insert(int x, XORList *ls)
+void insert(XORList *ls, int x)
 {
     Node *node = malloc(sizeof(Node));
     node->value = x;
@@ -39,21 +44,46 @@ void insert(int x, XORList *ls)
     }
     else
     {
-        Node* prev = ls->end;
+        Node *prev = ls->end;
         ls->end = node;
-        prev->xor_pointer = (int)prev->xor_pointer ^ (int)node;
+        prev->xor_pointer = ptr_xor(prev->xor_pointer, node);
         node->xor_pointer = prev; // next = 0, so xor_pointer = prev
     }
 }
 
-void map(int (*f)(int), XORList* ls){
+void foreach (XORList * ls, void (*f)(int))
+{
+    Node *next = ls->start;
+    if (ls->start == NULL)
+        return;
+    f(ls->start->value);
+    next = ptr_xor(next, next->xor_pointer);
+    while (next != ls->end)
+    {
+        f(next->value);
+        next = ptr_xor(next, next->xor_pointer);
+    }
+}
+
+void map(XORList *ls, int (*f)(int))
+{
     // TODO
+}
+
+void print_int(int x){
+    printf("%d\n",x);
 }
 
 int main(int argc, char const *argv[])
 {
-    Node n = {.value = 1, .xor_pointer = NULL};
+    XORList* ls = createEmptyList();
+    insert(ls, 1);
+    printf("Inserted 1\n");
+    insert(ls, 2);
+    printf("Inserted 2\n");
+    insert(ls,3);
+    printf("Inserted 3\n");
+    foreach(ls, print_int);
     printf("Hello\n");
-    printf("%d %p\n", n.value, n.xor_pointer);
     return 0;
 }
